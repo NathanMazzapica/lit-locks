4cbef75daec1e2271633a13a12441ff86daf8db9
Nathan Mazzapica <92413483+NathanMazzapica@users.noreply.github.com>
2023-05-31 14:54:44 -0400

    Init Commit

---
+++ sh_plugin.lua
@@ -0,0 +1,17 @@
+
+PLUGIN.name = "Lit Locks"
+PLUGIN.description = "Adds a dyanmic light to combine locks"
+PLUGIN.author = "Generic"
+
+
+
+-- create a config option for the light radius
+ix.config.Add("LockRadius", 128, "The radius of the light emitted by combine locks", nil, {
+    data = {min = 0, max = 384},
+    category = "Lit Locks"
+})
+
+ix.config.Add("LockBrightness", 0.5, "The brightness of the light emitted by combine locks", nil, {
+    data = {min = 0.0, max = 2, decimals = 1},
+    category = "Lit Locks"
+})
